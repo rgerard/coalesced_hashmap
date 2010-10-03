7095923c38e26b67909898553a28ebd22a64a7d0
Ryan Gerard <ryan.gerard@gmail.com>
2010-10-03 12:15:12 -0700

    First rough coalescing code

--- CoalescedHashMap.cpp
+++ CoalescedHashMap.cpp
@@ -1,29 +1,59 @@
 // This is the main DLL file.
 
 #include "CoalescedHashMap.h"
+#include <stdlib.h>
 
 CharacterMap::CharacterMap(int size) {
-	bucket = new Bucket[size];
-	m_size = size;
+	m_bucket = new Bucket[size];
+
+	m_actualsize = size;
+	m_restrictedsize = (size * m_percentrestrict);
+
 	for (int i = 0; i < size; i++) {
-		bucket[i].used = false;
+		m_bucket[i].used = false;
 	}
 }
 
 CharacterMap::~CharacterMap() {
-	delete [] bucket;
+	delete [] m_bucket;
 }
  
 bool CharacterMap::put(char key, long value) {
 	int index = hash(key);
 	
-	if(contains(key)) {
-		return false;	
+	// If the location is null, insert a new bucket to hashmap
+	if(m_bucket[index].used == false) {
+		Bucket new_bucket;
+		new_bucket.key = key;
+		new_bucket.valueOrPointer = value;
+		new_bucket.used = true;
+		m_bucket[index] = new_bucket;
+		return true;
 	}
 
-	bucket[index].key = key;
-	bucket[index].valueOrPointer = value;
-	bucket[index].used = true;
+	// Find the right location in the cellar for this new value, starting at (tablesize - 1)
+	int cursor = m_actualsize = 1;
+	while ( cursor >= 0 && m_bucket[cursor].used == false )
+      --cursor;
+
+	// Table is full, so return failure
+	if ( cursor == -1 )
+      return false;
+
+	// Insert new bucket at the cursor location
+	Bucket new_bucket;
+	new_bucket.key = key;
+	new_bucket.valueOrPointer = value;
+	new_bucket.used = true;
+	m_bucket[cursor] = new_bucket;
+
+	// Point the colliding node to this new node
+	Bucket it = m_bucket[index];
+ 
+	while ( it.next != NULL && it.used == true )
+      it = *(it.next);
+
+	it.next = &(m_bucket[cursor]);
 
 	return true;
 }
@@ -35,7 +65,7 @@ bool CharacterMap::get(char key, long &result) {
 		result = 0;
 		return false;
 	} else {
-		result = bucket[index].valueOrPointer;
+		result = m_bucket[index].valueOrPointer;
 		return true;
 	}
 }
@@ -46,12 +76,12 @@ bool CharacterMap::contains(char key) {
 }
 
 bool CharacterMap::contains(char key, int index) {
-	if(index >= m_size) {
+	if(index >= m_restrictedsize) {
 		return false;
 	}
 
 	// Check to see if the keys match
-	if(bucket[index].key == key) {
+	if(m_bucket[index].key == key) {
 		return true;
 	} else {
 		return false;
@@ -59,5 +89,5 @@ bool CharacterMap::contains(char key, int index) {
 }
 
 int CharacterMap::hash(char key) {
-	return key % m_size;
+	return key % m_restrictedsize;
 }
--- CoalescedHashMap.h
+++ CoalescedHashMap.h
@@ -2,20 +2,25 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
+static const float m_percentrestrict = 0.86;
+
 struct Bucket {
        bool used;
        char key;
        long valueOrPointer;
+	   Bucket *next;
 };
 
 class CharacterMap {
 private:
+       ~CharacterMap();
 	   int hash(char key);
-	   Bucket *bucket;
-	   int m_size;
+	   
+	   Bucket *m_bucket;
+	   int m_actualsize;
+	   int m_restrictedsize;
 public:
        CharacterMap(int size);
-       ~CharacterMap();       
        bool contains(char key);
 	   bool contains(char key, int index);
        bool put(char key, long value);
